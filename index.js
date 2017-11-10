ba8375fca5cd35791d9e698852d7db5cf12b617f
CamilaNuttoc <camila.canuto@outlook.com>
2017-11-10 20:54:35 -0200

    muda path de jquery-zoom

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ mongoose.connect("mongodb://localhost/ecommerce");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules"));
 
 app.get("/", function(req, res){
 	Item.find({}, function(err, itens){
@@ -34,8 +35,8 @@ app.get("/:id", function(req, res){
 // for (var i=0; i<12; i++){
 // 	var imagem = 'https://ph-cdn4.ecosweb.com.br/Web/posthaus/foto/moda-feminina/vestidos/vestido-floral-com-detalhe-transparente_180831_600_1.jpg';
 // 	var titulo = 'Vestido estampado preto';
-// 	var precoOriginal = 'R$49,90';
-// 	var precoPromocao = 'R$29,90';
+// 	var precoOriginal = 49.90;
+// 	var precoPromocao = 29.90;
 // 	var data = [{imagem, titulo, precoOriginal, precoPromocao}];
 // 	Item.create(data, function(err, item){
 // 	  	if(err){
