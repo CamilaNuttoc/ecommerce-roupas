dc5a08b7f04cea7088c867fc4e36e7cb25de862e
CamilaNuttoc <camila.canuto@outlook.com>
2017-11-14 14:08:24 -0200

    conserta radioBtn script

--- public/scripts/radioBtn.js
+++ public/scripts/radioBtn.js
@@ -2,19 +2,33 @@ $(document).ready(function(){
   $('[class*="cores"] .button > span').click(function(){
     var classe = $(this).attr('class').toString();
     var lastChar = classe.substr(classe.length-1);
-    $('[class*="cores'+lastChar+'"] .button > span').css('border', '2px solid #eee');
+    lastChar = Number(lastChar);
+    if(!isNaN(lastChar)){
+      alert(" equals to NaN");
+      $('[class*="cores'+lastChar+'"] .button > span').css('border', '2px solid #eee');
+    }else{
+      alert(" different than NaN");
+      $('.cores .button > span').css('border', '2px solid #eee');
+    }
     $(this).css('border', '2px solid #eee');
-    if($(this).hasClass('branco'+lastChar)){
+    if($(this).hasClass('branco'+lastChar) || $(this).hasClass('branco')){
       $(this).css('border', '8px solid #ccc');
     }else{
       $(this).css('border', '8px solid #fff');
     }
+    
   });
   $('[class*="tamanhos"] .button > span').click(function(){
     var classe = $(this).attr('class').toString();
     var lastChar = classe.substr(classe.length-1);
-    $('[class*="tamanhos'+lastChar+'"] .button > span > span').css('color', '#aaa');
-    $('[class*="tamanhos'+lastChar+'"] .button > span').css('background', '#ddd');
+    lastChar = Number(lastChar);
+    if(!isNaN(lastChar)){
+      $('[class*="tamanhos'+lastChar+'"] .button > span > span').css('color', '#aaa');
+      $('[class*="tamanhos'+lastChar+'"] .button > span').css('background', '#ddd');
+    }else{
+      $('.tamanhos .button > span > span').css('color', '#aaa');
+    $('.tamanhos .button > span').css('background', '#ddd');
+    }
     $(this).css('background', '#fff');
     $(this).find('span').css('color', '#000');
   });
