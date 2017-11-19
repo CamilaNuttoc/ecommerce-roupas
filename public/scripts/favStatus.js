04aac89266f64eb81f9d9c3b110dd6a363f85b4d
CamilaNuttoc <camila.canuto@outlook.com>
2017-11-19 01:10:15 -0200

    comenta e organiza favStatus

--- public/scripts/favStatus.js
+++ public/scripts/favStatus.js
@@ -1,15 +1,21 @@
+//Controla status de coração de favorito
 function favStatus(btn){
+
+  //Encontra 
   var icone = $(btn).find('.glyphicon');
   var texto = $(btn).find('.favText');
+
+  //Se não estiver favoritado...
   if(icone.hasClass('glyphicon-heart-empty')){
     icone.removeClass('glyphicon-heart-empty');
     icone.addClass('glyphicon-heart');
-    texto.fadeIn(0);
-    texto.animate({bottom: '35px'});
-    texto.fadeOut();
-  }else{
-    icone.addClass('glyphicon-heart-empty');
-    icone.removeClass('glyphicon-heart');
-    texto.css({bottom: '25px'});
+    texto.fadeIn(0).animate({bottom: '35px'}).fadeOut();
+    return;
   }
+  
+  ///Se estiver favoritado, desfavorita.
+  icone.addClass('glyphicon-heart-empty');
+  icone.removeClass('glyphicon-heart');
+  texto.css({bottom: '25px'});
+
 }
