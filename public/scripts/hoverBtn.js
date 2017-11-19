f3b28df480812abf67542fc0728221dbc0dd07a5
CamilaNuttoc <camila.canuto@outlook.com>
2017-11-19 01:13:28 -0200

    deleta script inútil

--- public/scripts/hoverBtn.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('.thumbnail').hover(function(){
-		$(this).find('.comprarBtn').addClass('btn-warning');
-	}, function(){
-		$(this).find('.comprarBtn').removeClass('btn-warning');
-	});
-});
