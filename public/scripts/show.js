e94ae881036df673778668009673a46fa7329e59
CamilaNuttoc <camila.canuto@outlook.com>
2017-11-19 19:45:57 -0200

    organiza script show.js

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,31 +1,62 @@
-$('.preview img').click(function(){
-	var novaImg = $(this).attr('src');
-	$('.main span img').attr('src', novaImg);
-});
 $(document).ready(function(){
-	$('.carrossel-container .content > div').slice(0, 1).clone().appendTo('.content2');
-  	$('.main span').zoom();
-})
-var avaliacoes = 0;
-var users = 0;
-avaliacoes = $('[class*="avaliacao-user"] .glyphicon-star').length-5;
-users = $('[class*="avaliacao-user"]').length-1;
-var length = Math.round(avaliacoes/users);
-genStars(length, -1, users);
 
-$(document).on('click', 'a[href^="#avaliacao"]', function (e) {
-  e.preventDefault();
-	$('html, body').animate({
-	  scrollTop: $($.attr(this, 'href')).offset().top
-	}, 800);
-});
+// ======================= Gera nota média de item ========================= //
+	
+	/* Reúne parâmetros para 
+   	   gerar nota média do item */
+	var avaliacoes = 0;
+	var users = 0;
+	
+	avaliacoes = $('[class*="avaliacao-user"] .glyphicon-star').length-5;
+	users = $('[class*="avaliacao-user"]').length-1;
+	
+	//Calcula média
+	var length = Math.round(avaliacoes/users);
+	
+	//Chama função para mostrar média na tela
+	genStars(length, -1, users);
 
-var dropUser = $('#dropdown-user');
 
-$('#abreLogin').click(function(e){
-	e.stopPropagation();
-	$('.icons:first-of-type > a').attr('aria-expanded', true);
-	$('.icons:first-of-type').addClass('open');
-	dropUser.hide();
-	dropUser.slideDown();
+/* ================= Copia parte de div 'sugestões' para
+   					 				 nova div (mostra mesmo conteúdo 
+   					 				 nos dois modos diferentes de display) =================*/
+
+	var sugestoes = $('.carrossel-container .content > div');
+	sugestoes.slice(0, 2).clone().appendTo('.content2');
+
+
+// ======================= Zoom em imagem principal ======================== //
+	$('.main span').zoom();
+
+
+/* ================== Muda imagem principal ao  
+   					          selecionar outra da área de preview ================== */
+
+	$('.preview img').click(function(){
+		var novaImg = $(this).attr('src');
+		$('.main span img').attr('src', novaImg);
+	});
+
+
+/* ===================== Anima scroll para avaliações ====================== */
+
+	$(document).on('click', 'a[href^="#avaliacao"]', function (e) {
+	  e.preventDefault();
+		$('html, body').animate({
+		  scrollTop: $($.attr(this, 'href')).offset().top
+		}, 800);
+	});
+
+
+/* ====================== Abre formulário de login  ======================== */
+
+	var drop = $('#dropdown-user');
+	$('#abreLogin').click(function(e){
+		e.stopPropagation();
+		$('.icons:first-of-type > a').attr('aria-expanded', true);
+		$('.icons:first-of-type').addClass('open');
+		drop.hide();
+		drop.slideDown();
+	});
+
 });
