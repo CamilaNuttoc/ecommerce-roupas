1dd7455f4de687a031faca0dddb1270303f10ed9
CamilaNuttoc <camila.canuto@outlook.com>
2017-11-21 21:05:33 -0200

    corrige indentação

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var mongoose = require("mongoose"),
-	Item = require("./models/item.js"),
-	Comentario = require("./models/comentario.js"),
-	bodyParser = require("body-parser"),
-	express = require("express"),
-	app = express();
+var mongoose   = require("mongoose"),
+	  Item       = require("./models/item.js"),
+	  Comentario = require("./models/comentario.js"),
+	  bodyParser = require("body-parser"),
+	  express    = require("express"),
+	  app        = express();
 
 mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -59,43 +60,43 @@ app.get("/:id", function(req, res){
 // 	var precoPromocao = 29.90;
 // 	var data = [{imagem, titulo, precoOriginal, precoPromocao}];
 // 	Item.create(data, function(err, item){
-// 	  	if(err){
-// 	    	console.log(err);
-// 	  	} else {
-// 	    	console.log('criou item');
-// 	  	}
+// 	  if(err){
+// 	   	console.log(err);
+// 	  } else {
+// 	   	console.log('criou item');
+// 	  }
 // 	});
 // }
 
 //Cria comentários com avaliações random
-// Item.find({}, function(err, itens){
-// 	if(err){
-// 		console.log(err);
-// 	} else {
-// 		itens.forEach(function(item){
-// 			var commentstest = [];
-// 			for(var j=0; j<4; j++){
-// 				var autor = 'Nome de usuário';
-// 				var avaliacao = Math.floor((Math.random() * 5) + 1);
-//     			var texto = 'Comentário demonstrando satisfação ou insatisfação!Curabitur congue faucibus magna quis ullamcorper. Vivamus eget nisl eu elit maximus tempus a quis eros. Suspendisse neque felis, ultrices et turpis ut, suscipit sagittis augue. Sed ac turpis dignissim, pharetra tellus nec, suscipit eros. Aliquam vitae massa risus. Cras at massa nec dolor posuere facilisis. Nunc facilisis ante quis consequat ultricies. Fusce finibus varius lacinia. Aliquam consectetur egestas nunc eget aliquam.';
-//     			var comentario = {autor: autor, avaliacao: avaliacao, texto: texto};
-//     			commentstest.push(comentario);
-//     		}
-//     			for(var i=0; i<commentstest.length; i++){
-// 				Comentario.create(commentstest[i], function(err, comentarioCriado){
-//     			    if(err){
-//     			        console.log(err);
-//     			    } else {
-//     			    	console.log(comentarioCriado);
-//     			      	item.comentarios.push(comentarioCriado);
-//     			      	console.log(item.comentarios)
-//     			   		item.save();
-// 				     }
-//     			});
-// 				}
-// 		});
-// 	}
-// });
+//Item.find({}, function(err, itens){
+//  if(err){
+// 	  console.log(err);
+//  } else {
+//    itens.forEach(function(item){
+//			var commentstest = [];
+//			for(var j=0; j<4; j++){
+//				var autor = 'Nome de usuário';
+//				var avaliacao = Math.floor((Math.random() * 5) + 1);
+//   			var texto = 'Comentário demonstrando satisfação ou insatisfação!Curabitur congue faucibus magna quis ullamcorper. Vivamus eget nisl eu elit maximus tempus a quis eros. Suspendisse neque felis, ultrices et turpis ut, suscipit sagittis augue. Sed ac turpis dignissim, pharetra tellus nec, suscipit eros. Aliquam vitae massa risus. Cras at massa nec dolor posuere facilisis. Nunc facilisis ante quis consequat ultricies. Fusce finibus varius lacinia. Aliquam consectetur egestas nunc eget aliquam.';
+//   			var comentario = {autor: autor, avaliacao: avaliacao, texto: texto};
+//   			commentstest.push(comentario);
+//   		}
+//   		for(var i=0; i<commentstest.length; i++){
+//				Comentario.create(commentstest[i], function(err, comentarioCriado){
+//   				if(err){
+//   				   console.log(err);
+//   				} else {
+//   				  console.log(comentarioCriado);
+//   				   	item.comentarios.push(comentarioCriado);
+//   				   	console.log(item.comentarios)
+//   				  item.save();
+//				  }
+//   			});
+//			}
+//	  });
+//  }
+//});
 
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("I'm working, yay!");
